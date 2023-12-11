9f00cecf23156a7199432b808bacb388c4cdc37b
Pablo Perales Molas <pperalesm@gmail.com>
2023-12-11 16:14:53 +0100

    Updated success color

--- src/app/components/proposals/proposals.component.ts
+++ src/app/components/proposals/proposals.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'dipnoi-main',
+  selector: 'dipnoi-proposals',
   standalone: true,
   templateUrl: './proposals.component.html',
   styleUrl: './proposals.component.scss',
