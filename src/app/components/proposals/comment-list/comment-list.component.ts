7f2da73337f5e4ce46fa60e479e8041befbb4fcd
Pablo Perales Molas <pperalesm@gmail.com>
2024-01-26 13:47:09 +0100

    Added signed in listener to comments

--- src/app/components/proposals/comment-list/comment-list.component.ts
+++ src/app/components/proposals/comment-list/comment-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { CommentOrderByEnum, OrderEnum } from '../../../constants/enums';
 import { InsufficientItemsDirective } from '../../../directives/insufficient-items.directive';
 import { CommentService } from '../../../services/comment.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'dipnoi-comment-list',
@@ -12,25 +14,35 @@ import { CommentService } from '../../../services/comment.service';
   styleUrl: './comment-list.component.scss',
   imports: [CommonModule, InfiniteScrollModule, InsufficientItemsDirective],
 })
-export class CommentListComponent implements OnInit {
+export class CommentListComponent implements OnInit, OnDestroy {
   @Input({ required: true }) infiniteScrollContainerRef!: HTMLElement;
   @Input({ required: true }) proposalId!: number;
 
+  signedIn$!: Subscription;
   params!: {
     proposalId: number;
     orderBy?: CommentOrderByEnum;
     order?: OrderEnum;
   };
 
-  constructor(public commentService: CommentService) {}
+  constructor(
+    public commentService: CommentService,
+    public authService: AuthService,
+  ) {}
 
   ngOnInit() {
     this.params = { proposalId: this.proposalId };
 
-    this.commentService.reset(this.params).subscribe();
+    this.signedIn$ = this.authService.signedIn.subscribe(() => {
+      this.commentService.reset(this.params).subscribe();
+    });
   }
 
   onScrollEnd() {
     this.commentService.readMore(this.params)?.subscribe();
+  }
+
+  ngOnDestroy() {
+    this.signedIn$.unsubscribe();
   }
 }
