dc3dac8a474f54934e1b609c0da003ea0c68dc1e
Pablo Perales Molas <pperalesm@gmail.com>
2024-02-13 11:43:00 +0100

    Added conditional categories

--- src/app/components/proposals/proposal-card/proposal-card.component.ts
+++ src/app/components/proposals/proposal-card/proposal-card.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  OnDestroy,
   OnInit,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -9,6 +10,8 @@ import { Proposal } from '../../../models/proposal.model';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { RoutePathEnum } from '../../../app.routes';
 import { ProposalStateEnum } from '../../../constants/enums';
+import { UserService } from '../../../services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'dipnoi-proposal-card',
@@ -18,24 +21,51 @@ import { ProposalStateEnum } from '../../../constants/enums';
   imports: [CommonModule, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProposalCardComponent implements OnInit {
+export class ProposalCardComponent implements OnInit, OnDestroy {
   @Input({ required: true }) proposal!: Proposal;
 
+  authUser$!: Subscription;
+  isPendingSpecification!: boolean;
+  isLastCall!: boolean;
+  isSelectedForDevelopment!: boolean;
+  isInDevelopment!: boolean;
   positiveRatio!: number;
   totalValue!: number;
   isInBacklog!: boolean;
   importanceTag!: string;
 
-  constructor(public route: ActivatedRoute) {}
+  constructor(
+    public route: ActivatedRoute,
+    public userService: UserService,
+  ) {}
 
   ngOnInit() {
+    this.authUser$ = this.userService.authUser$.subscribe({
+      next: (authUser) => {
+        this.isPendingSpecification =
+          this.proposal.state === ProposalStateEnum.PENDING_SPECIFICATION &&
+          this.proposal.user.id === authUser?.id;
+      },
+    });
+
+    this.isLastCall = this.proposal.state === ProposalStateEnum.LAST_CALL;
+
+    this.isSelectedForDevelopment =
+      this.proposal.state === ProposalStateEnum.SELECTED_FOR_DEVELOPMENT;
+
+    this.isInDevelopment =
+      this.proposal.state === ProposalStateEnum.IN_DEVELOPMENT;
+
     this.totalValue = this.proposal.positiveValue + this.proposal.negativeValue;
+
     this.positiveRatio = this.totalValue
       ? Math.round((100 * this.proposal.positiveValue) / this.totalValue)
       : 0;
+
     this.isInBacklog =
       this.proposal.state === ProposalStateEnum.SELECTED_FOR_DEVELOPMENT ||
       this.proposal.state === ProposalStateEnum.IN_DEVELOPMENT;
+
     this.importanceTag =
       this.proposal.importance > 4
         ? 'MAX'
@@ -50,5 +80,9 @@ export class ProposalCardComponent implements OnInit {
 
   buildSelectedQueryParam() {
     return { [RoutePathEnum.PROPOSAL]: this.proposal.id };
+  }
+
+  ngOnDestroy() {
+    this.authUser$.unsubscribe();
   }
 }
